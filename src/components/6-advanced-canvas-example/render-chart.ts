b6a15262883294e93fe31137ed4aef0033060b91
Didrik Fleischer <didrik.fleischer@gmail.com>
2021-02-26 00:33:11 +0100

    chore - remove unused code

--- src/components/6-advanced-canvas-example/render-chart.ts
+++ src/components/6-advanced-canvas-example/render-chart.ts
@@ -2,60 +2,7 @@ import * as d3 from "d3";
 
 import { Scales } from "./D3CanvasChart";
 import { PointData } from "../../types";
-import { MARGIN, TRANSITION_DURATION } from "../../statics";
-
-/**
- * Render bars in the stacked bar chart.
- */
-export function renderBarChart(
-  scales: Scales,
-  data: PointData[],
-  height: number
-): void {
-  const dataContainer = d3.select("sketch");
-
-  dataContainer
-    .selectAll("rect")
-    .data(data, (d) => `${(d as PointData).key}-${(d as PointData).category}`)
-    .join(
-      (enter) =>
-        // @ts-ignore
-        enter
-          .append("rect")
-          .attr("x", (d) => scales.x(d.key + ""))
-          .attr("y", (d) => scales.y(d.value))
-          .attr("height", (d) => {
-            return height - MARGIN.bottom - (scales.y(d.value) as number);
-          })
-          .attr("width", scales.x.bandwidth())
-          .attr("opacity", 0)
-          .call((_enter) =>
-            _enter.transition().duration(TRANSITION_DURATION).attr("opacity", 1)
-          ),
-      (update) =>
-        update.call((update) =>
-          // @ts-ignore
-          update
-            .transition()
-            .duration(TRANSITION_DURATION)
-            .attr("x", (d) => scales.x(d.key + ""))
-            .attr("y", (d) => scales.y(d.value))
-            .attr("height", (d) => {
-              return height - MARGIN.bottom - (scales.y(d.value) as number);
-            })
-            .attr("width", scales.x.bandwidth())
-            .attr("opacity", 1)
-        ),
-      (exit) =>
-        exit.call((exit) =>
-          exit
-            .transition()
-            .duration(TRANSITION_DURATION)
-            .attr("opacity", 0)
-            .remove()
-        )
-    );
-}
+import { TRANSITION_DURATION } from "../../statics";
 
 /**
  * Render points in the scatter chart.
